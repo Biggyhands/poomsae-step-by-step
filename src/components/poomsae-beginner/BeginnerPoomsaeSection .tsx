62b63324674ac8dd2083418769295708c6383585
Robert <robert.alejandro.manotas@gmail.com>
2025-06-26 13:34:04 -0500

    Update BeginnerPoomsaeSection .tsx

--- src/components/poomsae-beginner/BeginnerPoomsaeSection .tsx
+++ src/components/poomsae-beginner/BeginnerPoomsaeSection .tsx
@@ -33,7 +33,7 @@ const poomsaeCardsData = [
 
 function BeginnerPoomsaeSection() {
   return (
-    // Contenedor principal con el padding superior reducido
+    // Contenedoaar principal con el padding superior reducido
     <section className="pt-12 pb-24 bg-gray-50 dark:bg-gray-900">
       <div className="text-center mb-12">
         <h2 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white">
